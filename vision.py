af196e55cd48d8df996f5d958be5585c6e70f302
HoangQuan28 <quandinhhoang560@gmail.com>
2024-05-15 22:45:45 +0700

    Update vision.py
    hquan

--- vision.py
+++ vision.py
@@ -50,6 +50,27 @@ with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0
     cap.release()
     cv2.destroyAllWindows()
 plt.imshow(cv2.cvtColor(frame,cv2.Color_BGR2RGB))
+len(results.left_hand_landmarks.landmark)
+for res in results.pose_landmarks.landmark:
+    test = np.array([res.x, res.y, res.z, res.visibility])
+    pose.append(test)
+pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
+lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() 
+    if results.face_landmarks
+    else np.zeros(1404)
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, face, lh, rh])
+result_test = extract_keypoints(results)
+result_test
+np.save('0', result_test)
+np.load('0.npy')
 
 
 
